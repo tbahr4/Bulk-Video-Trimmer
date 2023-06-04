1863ac155e789f71b297fc8996975d15c2f263bc
Taylor Bahr <tbahr00@gmail.com>
2023-06-04 15:14:53 -0500

    volume, volume bar, mute

--- video.py
+++ video.py
@@ -12,7 +12,7 @@ WINDOW_WIDTH = 1024
 
 
 class VideoPlayer(tk.Frame):
-    def __init__(self, parent, screenWidth: int, screenHeight: int, playOnOpen: bool):
+    def __init__(self, parent, screenWidth: int, screenHeight: int, playOnOpen: bool, backgroundHeight: int):
         """
             Params:
             screenWidth: the width of the video screen
@@ -21,6 +21,8 @@ class VideoPlayer(tk.Frame):
         """
         super().__init__(parent)
         self.parent = parent
+        self.lastVolumeChange = 0  
+        self.isVolumeBarVisible = False
 
         # properties
         self.playOnOpen = playOnOpen
@@ -37,10 +39,15 @@ class VideoPlayer(tk.Frame):
 
         # progress bar
         progressBarHeight = 5
-        self.progressBar = ProgressBar(self, self.player, width=screenWidth, height=progressBarHeight, bg='#383838', fg='#4287f5', cursorRadius=10)
+        self.progressBar = ProgressBar(self, self.player, width=screenWidth, height=progressBarHeight, bg='#383838', fg='#4287f5')
         # buttons
-        self.actionBar = ActionBar(self, self.player, 25, self.progressBar, progressBarHeight)
+        padX = 5
+        self.volumeBar = VolumeBar(self, player=self.player, defaultVolume=50, width=19, height=50)
+        self.actionBar = ActionBar(self, self.player, buttonSize=25, progressBar=self.progressBar, volumeBar=self.volumeBar, progressBarHeight=progressBarHeight, padX=padX)
         self.bPause = self.actionBar.bPause
+        # background
+        self.background = tk.Canvas(self, width=WINDOW_WIDTH, height=screenHeight+backgroundHeight, bg='black', borderwidth=0, highlightthickness=0)
+        
 
         
 
@@ -50,16 +57,24 @@ class VideoPlayer(tk.Frame):
         
 
         # display elements
+        self.background.place(x=0, y=0)
         self.canvas.pack()
         self.progressBar.place(x=0, y=screenHeight)
-        self.actionBar.place(x=5, y=screenHeight)
+        self.actionBar.place(x=5, y=screenHeight+(progressBarHeight*2))
+        root.update()  # update to get positions of button widgets 
+        self.volumeBar.place(x=self.actionBar.bVolume.winfo_x() + 8, y=screenHeight - 55, width=self.volumeBar.width, height=self.volumeBar.height)
+        self.actionBar.lift()
         self.progressBar.lift()
-        self.bg = self.canvas.create_rectangle(0,0, WINDOW_WIDTH, WINDOW_HEIGHT, fill="orange", outline="")
+        self.volumeBar.lift()
+        
 
         # add listener for events
         root.bind("<KeyPress>", self.onKeyPress)
         self.progressBar.bind("<Enter>", self.onHover_ProgressBar)
         self.progressBar.bind("<Leave>", self.onLeave_ProgressBar)
+
+    def toggleFullscreen(self):
+        self.parent.attributes("-fullscreen", not self.parent.attributes("-fullscreen"))
 
     def onHover_ProgressBar(self, event):
         self.progressBar.place(x=0, y=self.screenHeight - self.progressBar.height)
@@ -77,11 +92,20 @@ class VideoPlayer(tk.Frame):
         elif key == "Right":
             self.seek(10000)
         elif key == "Up":
-            self.volume += 1 if self.volume < 100 else 0
+            self.volume = min(100, self.volume + 5)
             self.player.audio_set_volume(self.volume)
+            self.actionBar.bVolume.setVolume(self.volume)
+            self.actionBar.bVolume.unmute()
+            self.volumeBar.setValue(self.volume)
+            self.lastVolumeChange = time.time()
+
         elif key == "Down":
-            self.volume -= 1 if self.volume > 0 else 0
+            self.volume = max(0, self.volume - 5)
             self.player.audio_set_volume(self.volume)
+            self.actionBar.bVolume.setVolume(self.volume)
+            self.actionBar.bVolume.unmute()
+            self.volumeBar.setValue(self.volume)
+            self.lastVolumeChange = time.time()
         elif key == "period":
             if self.bPause.isPaused:
                 fps = self.player.get_fps()
@@ -174,6 +198,25 @@ class VideoPlayer(tk.Frame):
         if position != 0:       # do not update on restart video
             self.progressBar.setValue(1 if playState == vlc.State.Ended else position)
 
+        # update last hover time if currently hovering
+        if self.volumeBar.isHovering:
+            self.volumeBar.lastVolumeHover = time.time()
+        if self.actionBar.bVolume.isHovering:
+            self.actionBar.bVolume.lastVolumeHover = time.time()
+
+        # update volume bar visibility
+        isHovering = self.volumeBar.isHovering or self.actionBar.bVolume.isHovering
+        timeSinceLastVolChange = time.time() - self.lastVolumeChange
+        timeSinceLastVolHover = time.time() - max(self.volumeBar.lastVolumeHover, self.actionBar.bVolume.lastVolumeHover)
+
+        if (timeSinceLastVolHover < 1 or timeSinceLastVolChange < 1):
+            if not self.isVolumeBarVisible:
+                self.isVolumeBarVisible = True
+                self.volumeBar.place(x=self.actionBar.bVolume.winfo_x() + 8, y=self.screenHeight - 55, width=self.volumeBar.width, height=self.volumeBar.height)
+        else:   # hide
+            self.isVolumeBarVisible = False
+            self.volumeBar.place(x=self.actionBar.bVolume.winfo_x() + 8, y=self.screenHeight - 55, width=0, height=0)
+
         # Schedule the next update
         self.after(10, self.scheduleUpdates)
 
@@ -182,52 +225,195 @@ class ActionBar(tk.Frame):
     """
         A frame of buttons used to control the video player
     """
-    def __init__(self, parent, player, buttonSize: int, progressBar, progressBarHeight: int):
+    def __init__(self, parent, player, buttonSize: int, progressBar, volumeBar, progressBarHeight: int, padX: int):
         super().__init__(parent, bg="black")
         self.parent = parent
         self.player = player
         self.buttonSize = buttonSize
         self.progressBar = progressBar
-        padX = 5
+        self.volumeBar = volumeBar
 
         # init elements
         self.bPause = PauseButton(self, self.player, size=buttonSize)
         self.bSkipBackward = SkipButton(self, self.player, self.progressBar, pauseButton=self.bPause, isForwardSkip=False, size=buttonSize)
         self.bSkipForward = SkipButton(self, self.player, self.progressBar, pauseButton=self.bPause, isForwardSkip=True, size=buttonSize)
-        self.bVolume = VolumeButton(self, defaultVolume=50, size=buttonSize)
+        self.bVolume = VolumeButton(self, player=self.player, volumeBar=self.volumeBar, defaultVolume=50, size=buttonSize)
+        self.bFullscreen = FullscreenButton(self, root=root, size=buttonSize)
 
         # display
-        self.bPause.grid(column=0, row=0, padx=padX, pady=progressBarHeight*2)
+        self.bPause.grid(column=0, row=0, padx=padX)
         self.bSkipBackward.grid(column=1, row=0, padx=padX)
         self.bSkipForward.grid(column=2, row=0, padx=padX)
         self.bVolume.grid(column=3, row=0, padx=padX)
-
-        self.after(100, self.retrieve_coordinates)
-
-    def retrieve_coordinates(self):
-        # Print coordinates of the PauseButton relative to the root window
-        print("PauseButton Coordinates:", self.bPause.winfo_x(), self.bPause.winfo_y())
+        self.bFullscreen.grid(column=4, row=0, padx=padX)
 
 
+class FullscreenButton(tk.Frame):
+    """
+        A button for setting and exiting window fullscreen
+    """
+    def __init__(self, parent, root, size: int = 50):
+        super().__init__(parent)
+        self.root = root
+
+        image = Image.open("images/fullscreen.png")
+        image.thumbnail((size, size))
+        self.image = ImageTk.PhotoImage(image)
+        self.bVolume = tk.Button(self, width=size, command=self.toggleFullscreen, image=self.image, borderwidth=0, highlightthickness=0, bg="black", activebackground="black")
+
+        self.bVolume.pack()
+
+    def toggleFullscreen(self):
+        self.root.attributes("-fullscreen", not self.root.attributes("-fullscreen"))
+
+class VolumeBar(tk.Frame):
+    """
+        A volume button for muting and changing output volume
+    """
+    def __init__(self, parent, player, defaultVolume: int, width: int, height: int):
+        super().__init__(parent)
+        self.parent = parent
+        self.player = player
+        self.width = width
+        self.height = height
+        self.volume = defaultVolume
+        self.isHovering = False
+        self.lastVolumeHover = 0
+
+        self.canvas = tk.Canvas(self, width=width, height=height, borderwidth=0, highlightthickness=0, bg='#0F0F0F')
+
+        center = int(self.width / 2)
+        radius = 2.5
+        self.verticalSpacing = 5
+        self.backBar = self.canvas.create_rectangle(center-radius, self.verticalSpacing, center+radius, self.height-self.verticalSpacing, fill="#383838", outline='')
+
+        # volume bar
+        percent = defaultVolume / 100
+        vPos = (1-percent) * (self.height - (2*self.verticalSpacing)) + self.verticalSpacing
+        self.volumeBar = self.canvas.create_rectangle(center-radius, vPos, center+radius, self.height-self.verticalSpacing, fill="#4287f5", outline='')
+
+        self.canvas.pack()
+
+        # listeners
+        self.bind("<Enter>", self.onHover)
+        self.bind("<Leave>", self.onLeave)
+        self.canvas.bind("<B1-Motion>", self.onDrag)
+        self.canvas.bind("<Button-1>", self.onClick)
+
+    def onDrag(self, event):
+        y = event.y - self.canvas.canvasy(0) - self.verticalSpacing
+        percent = 1 - (y / (self.height - (2*self.verticalSpacing)))
+        percent = max(0, percent)
+        percent = min(1, percent)
+
+        volume = percent * 100
+        self.setValue(volume)
+        self.volume = int(volume)
+        self.parent.volume = self.volume
+        self.player.audio_set_volume(self.volume)
+        self.parent.actionBar.bVolume.setVolume(self.volume)
+        self.parent.actionBar.bVolume.unmute()
+        self.parent.lastVolumeChange = time.time()
+
+    def onClick(self, event):
+        self.onDrag(event)
+
+    def setValue(self, volume: float):
+        if volume > 100 or volume < 0: return
+        self.volume = volume
+
+        # if 0, just hide the rectangle
+        self.canvas.itemconfigure(self.volumeBar, state="hidden" if self.volume == 0 else "normal")
+
+        x1, y1, x2, y2 = self.canvas.coords(self.volumeBar)
+        percent = self.volume / 100
+        vPos = (1-percent) * (self.height - (2*self.verticalSpacing)) + self.verticalSpacing
+        self.canvas.coords(self.volumeBar, x1, vPos, x2, y2)
+
+    def onHover(self, event):
+        self.isHovering = True
+        self.lastVolumeHover = time.time()
+
+    def onLeave(self, event):
+        self.isHovering = False
+
+
+    
 
 class VolumeButton(tk.Frame):
     """
         A volume button for muting and changing output volume
     """
-    def __init__(self, parent, defaultVolume: int, size: int = 50):
+    def __init__(self, parent, player, volumeBar: VolumeBar, defaultVolume: int, size: int):
         super().__init__(parent)
+        self.player = player
         self.volume = defaultVolume
-
-        image = Image.open("images/volume.png")
-        image.thumbnail((size, size))
-        self.image = ImageTk.PhotoImage(image)
-
-        self.bVolume = tk.Button(self, width=size, command=self.toggleMute, image=self.image, borderwidth=0, highlightthickness=0, bg="black", activebackground="black")
+        self.isHovering = False
+        self.timeToShowSlider = 300
+        self.volumeBar = volumeBar
+        self.lastVolumeHover = 0
+        self.isMuted = False
+        
+        #images
+        images = [Image.open("images/volume-mute.png"), Image.open("images/volume-min.png"), Image.open("images/volume-mid.png"), Image.open("images/volume-max.png")]
+        for image in images: image.thumbnail((size, size))
+        self.images = [ImageTk.PhotoImage(image) for image in images]
+        self.bVolume = tk.Button(self, width=size, command=self.toggleMute, borderwidth=0, highlightthickness=0, bg="black", activebackground="black")
+        self.updateImage()
 
         self.bVolume.pack()
 
+        # listeners
+        self.bind("<Enter>", self.onHover)
+        self.bind("<Leave>", self.onLeave)
+
     def toggleMute(self):
-        pass
+        self.isMuted = not self.isMuted
+        self.mute() if self.isMuted else self.unmute()
+
+    def setVolume(self, value):
+        """
+            Used for setting the volume bar for after unmuting
+        """
+        if value < 0 or value > 100: return
+        self.volume = value
+        self.updateImage()
+
+
+    def updateImage(self):
+        if self.isMuted:
+            self.bVolume.config(image=self.images[0])
+        elif self.volume == 0:
+            self.bVolume.config(image=self.images[1])
+        elif self.volume < 50:
+            self.bVolume.config(image=self.images[2])
+        else:
+            self.bVolume.config(image=self.images[3])
+
+
+    def mute(self):
+        self.isMuted = True
+        self.player.audio_set_volume(0)
+        self.volumeBar.setValue(0)
+        self.lastVolumeChange = time.time()
+        self.updateImage()
+        
+
+    def unmute(self):
+        self.isMuted = False
+        self.player.audio_set_volume(self.volume)
+        self.volumeBar.setValue(self.volume)
+        self.lastVolumeChange = time.time()
+        self.updateImage()
+
+    def onHover(self, event):
+        self.isHovering = True
+        self.lastVolumeHover = time.time()
+
+    def onLeave(self, event):
+        self.isHovering = False
+
+        
 
 
 class PauseButton(tk.Frame):
@@ -322,7 +508,7 @@ class SkipButton(tk.Frame):
         
 
 class ProgressBar(tk.Frame):
-    def __init__(self, parent, player, width: int, height: int, bg: str, fg: str, cursorRadius: int):
+    def __init__(self, parent, player, width: int, height: int, bg: str, fg: str):
         super().__init__(parent)  
         self.parent = parent
         self.width = width
@@ -439,7 +625,7 @@ if __name__ == "__main__":
     root.geometry(str(WINDOW_WIDTH) + "x" + str(WINDOW_HEIGHT))
     root.resizable(width=False, height=False)
 
-    video = VideoPlayer(root, screenWidth=WINDOW_WIDTH, screenHeight=int(1080/2), playOnOpen=False)
+    video = VideoPlayer(root, screenWidth=WINDOW_WIDTH, screenHeight=int(1080/2), playOnOpen=False, backgroundHeight=40)
     video.place(x=0,y=0, width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
 
 
