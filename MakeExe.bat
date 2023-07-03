2e86500c3abbdf5b1826c55a7bc5f5e021fafe60
Taylor Bahr <tbahr00@gmail.com>
2023-07-02 20:04:32 -0500

    added .bat for creating/removing exe

---
+++ MakeExe.bat
@@ -0,0 +1,7 @@
+pyinstaller --add-data "images;images" -F -w --additional-hooks-dir=. -i images\logo.ico main.py -n "Bulk Video Trimmer"
+
+set "dest=dist"
+robocopy "plugins" "%dest%\plugins" /E
+copy "libvlc.dll" "%dest%"
+copy "libvlccore.dll" "%dest%"
+pause
